5ef892f125f37dc15c0f38dd425a43837d42a78a
sendlan <9282930+sendlan@users.noreply.github.com>
2022-03-03 19:46:52 +0800

    Update diy-2.sh

--- script/diy-2.sh
+++ script/diy-2.sh
@@ -3,17 +3,8 @@
 # 修改openwrt登陆地址,把下面的192.168.2.1修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# 修改主机名字，把E8820S修改你喜欢的就行（不能纯数字或者使用中文）
+# 修改主机名字，把Openwrt修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/OpenWrt/CMCC-A9/g' package/base-files/files/bin/config_generate
-
-# 修改开源驱动wifi名称
-sed -i 's/OpenWrt/ZCMCC-A9/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-# 修改闭源驱动2G wifi名称
-#sed -i 's/OpenWrt_2G/ZTE-E8820S/g' package/lean/mt/drivers/mt_wifi/files/mt7603.dat
-
-# 修改闭源驱动5G wifi名称
-#sed -i 's/OpenWrt_5G/ZTE-E8820S-5G/g' package/lean/mt/drivers/mt_wifi/files/mt7612.dat
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
