4d99a9ab53a1630fce8471bc8129b23d36e832f5
sendlan <9282930+sendlan@users.noreply.github.com>
2022-03-25 10:36:12 +0800

    Create diy-3.sh

---
+++ script/diy-3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# 修改openwrt登陆地址,把下面的192.168.2.1修改成你想要的就可以了
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字，把Openwrt修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i 's/OpenWrt/CMCC-A9/g' package/base-files/files/bin/config_generate
+
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
+
+# 更改时区
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+#修正连接数
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
